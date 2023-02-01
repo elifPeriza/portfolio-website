71384d9885266df3db32adfca961180150cf1aa0
elif <78274404+elifPeriza@users.noreply.github.com>
2023-02-02 00:42:12 +0100

    Homepage layout (#3)
    * corrected  components for markdown renderer
    
    * created general layout for pages

--- frontend/features/markdown/ArticleImage.tsx
+++ frontend/features/markdown/ArticleImage.tsx
@@ -1,8 +1,8 @@
 export const ArticleImage = ({ src, alt, title }: any) => {
   return (
-    <span className="relative w-full block ">
+    <span className="relative w-full block my-6 ">
       <img
-        className="rounded-[20px] object-cover h-full w-full drop-shadow-lg"
+        className="rounded-[20px] object-cover h-full w-full  drop-shadow-lg"
         src={src}
         alt={alt}
         title={title}
