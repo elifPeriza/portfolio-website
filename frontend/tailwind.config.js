76402ae5a839f86fd4ccbe3cdd5bf1ea01f59c02
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-01 21:51:00 +0100

    added theme to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+      noto: ["Noto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "violet-primary": "#806CFF",
+        "neon-primary": "#D3FA48",
+        "neon-light": "#E4FB94",
+        "violet-secondary": "#6C52FF",
+        black: "#302518",
+        "violet-bg": "#FAFAFF",
+      },
+    },
   },
+
   plugins: [],
 };
