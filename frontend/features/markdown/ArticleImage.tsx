cad3a7ebdbb6d3dc8c5065bbdaba13843eb9b845
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-01 14:37:11 +0100

    tailwind config & test

--- frontend/features/markdown/ArticleImage.tsx
+++ frontend/features/markdown/ArticleImage.tsx
@@ -3,12 +3,10 @@ import Image from "next/image";
 export const ArticleImage = ({ src, alt, title }: any) => {
   return (
     <img
-      style={{ borderRadius: "20px" }}
+      className="rounded-[15px] h-24 w-52"
       src={src}
       alt={alt}
       title={title}
-      width={200}
-      height={100}
     ></img>
   );
 };
