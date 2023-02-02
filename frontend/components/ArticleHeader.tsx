99c42423390971d77f0663b99423ebbf7cf88d3b
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-02 21:52:49 +0100

    implemented article page

--- frontend/components/ArticleHeader.tsx
+++ frontend/components/ArticleHeader.tsx
@@ -1,14 +1,14 @@
-export default function ArticleHeader() {
+export default function ArticleHeader({ date, title, keywords }: any) {
   return (
     <div className="pt-20 sm:pt-32">
       <h3 className="font-robotoMono text-violet-secondary text-sm mb-4 ">
-        14-02-2023
+        {date}
       </h3>
       <h1 className="font-poppins font-bold text-black text-2xl  mb-3 sm:text-3xl">
-        The history of programming from a gender perspective
+        {title}
       </h1>
       <h4 className="font-robotoMono text-violet-secondary text-sm mb-3 ">
-        #gender, #inclusivity, #history
+        {keywords}
       </h4>
       <hr className="border-t-2 border-violet-primary mb-8"></hr>
     </div>
