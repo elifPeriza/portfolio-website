0d3b8d48342656767b1a278ffeb8c8c65f8cbbdc
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-02 15:10:15 +0100

    add bg color article page

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Button({ href, children }: any) {
   return (
     <Link href={href} target="_blank" rel="noreferrer">
-      <button className="font-noto font-bold text-[0.925rem] text-violet-primary border-2 border-violet-primary rounded-md px-3 py-1 ">
+      <button className="font-noto bg-white font-bold text-[0.925rem] text-violet-primary border-2 border-violet-primary rounded-md px-3 py-1 ">
         {children}
       </button>
     </Link>
