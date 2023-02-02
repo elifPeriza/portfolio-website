76b049c43242578fe732910db4c2598be08cc25d
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-02 13:21:41 +0100

    created responsive article card component

---
+++ frontend/components/Tag.tsx
@@ -0,0 +1,7 @@
+export default function Tag({ children }: any) {
+  return (
+    <div className="inline-block bg-neon-light px-3 py-1 font-robotoMono text-sm">
+      {children}
+    </div>
+  );
+}
