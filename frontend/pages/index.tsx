3896e6493518ba1c8e1aac58edf9c3e11c4834d6
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-02 11:37:35 +0100

    added navbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 export default function Web() {
   return (
     <div className=" mx-auto px-[5%] max-w-[1030px]">
-      <div className="border">Home</div>
+      <Navbar />
+      {/* <div className="border">Home</div> */}
       <Header />
     </div>
   );
