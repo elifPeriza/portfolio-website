9ef5676881a8c593e1856433a5f8060b1a7295c4
Elif Gömleksiz <e.goemlek@gmail.com>
2023-02-02 11:06:54 +0100

    added responsive header

--- frontend/features/markdown/ArticleParagraph.tsx
+++ frontend/features/markdown/ArticleParagraph.tsx
@@ -1,6 +1,6 @@
 export const ArticleParagraph = ({ children }: any) => {
   return (
-    <p className=" font-noto font-normal text-base text-black mb-4 sm:text-lg sm:mb- ">
+    <p className=" font-noto font-normal text-base text-black mb-4 sm:text-lg sm:mb-6 ">
       {children}
     </p>
   );
