063d204e8e35cd6d5aaa1d5a3bc1c425cd48432c
Elif Gömleksiz <e.goemlek@gmail.com>
2023-03-05 19:47:37 +0100

    change logo placeholder

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 export default function Navbar() {
   return (
     <div className="flex flex-row justify-between items-center py-6 ">
-      <h2 className="font-poppins font-bold text-4xl text-violet-primary">
-        LOGO
+      <h2 className="font-poppins font-bold text-4xl text-black">
+        EG<span className="text-violet-secondary">.</span>
       </h2>
       <Button
         href={`${backendURL}/static/files/resume.pdf`}
