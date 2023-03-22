625f18e5e8453b70caaa138f2441e87f56120f5b
Elif Gömleksiz <e.goemlek@gmail.com>
2023-03-22 13:02:59 +0100

    add comments to component for code highlighting

--- frontend/features/markdown/ArticleCode.tsx
+++ frontend/features/markdown/ArticleCode.tsx
@@ -4,11 +4,15 @@ import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
 import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
 
+// With the light build  we need to import and register the TypeScript and TSX languages with the syntax highlighter
 SyntaxHighlighter.registerLanguage("tsx", tsx);
 SyntaxHighlighter.registerLanguage("typescript", typescript);
 
 export const ArticleCode = ({ inline, className, children, ...props }: any) => {
+  // Extract the language from the className using a regular expression
   const match = /language-(\w+)/.exec(className || "");
+
+  // Render a code block with syntax highlighting if inline is false
   return !inline ? (
     <SyntaxHighlighter
       children={String(children).replace(/\n$/, "")}
