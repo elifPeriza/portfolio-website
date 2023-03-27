a7b3336fe3cf90eee8642c7ae6be3b90f24f87c2
Elif Gömleksiz <e.goemlek@gmail.com>
2023-03-27 19:01:29 +0200

    possible fix for squircle image on ios

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -14,18 +14,18 @@ export default function Header() {
           </clipPath>
         </defs>
       </svg>
-      <div className="flex flex-row gap-4 items-center py-7  mt-8 headerBreakpoint:px-0 sm:mt-16">
+      <div className="mt-8 flex flex-row items-center gap-4  py-7 sm:mt-16 headerBreakpoint:px-0">
         <div className="flex flex-col ">
-          <div className="flex flex-row gap-4 mb-5 items-center ">
-            <h1 className="font-poppins font-bold text-black text-2xl  mb-3 sm:text-3xl">
+          <div className="mb-5 flex flex-row items-center gap-4 ">
+            <h1 className="mb-3 font-poppins text-2xl font-bold  text-black sm:text-3xl">
               Hi, I’m Elif
               <Dot variant="violet" />
               <br /> Front-End Developer
             </h1>
-            <div className="hidden squircle bg-[url('/elif2.webp')] bg-cover bg-center aspect-square flex-1 min-w-[130px] max-w-[150px] headerTablet:block"></div>
+            <div className="squircle hidden aspect-square h-[142px] w-[142px] bg-[url('/elif2.webp')] bg-cover bg-center headerTablet:block"></div>
           </div>
 
-          <p className="font-inter font-normal text-base text-black mb-4 sm:text-lg sm:mb-6 ">
+          <p className="mb-4 font-inter text-base font-normal text-black sm:mb-6 sm:text-lg ">
             I am passionate about building things on the internet that not only
             look great, but provide a seamless and enjoyable user experience.
           </p>
@@ -33,7 +33,7 @@ export default function Header() {
             <a
               href="https://www.linkedin.com/in/elifgoemleksiz/"
               rel="noreferrer"
-              className="w-7 h-7"
+              className="h-7 w-7"
             >
               <Image src={LinkedInLogo} alt="Go to my LinkedIn" />
             </a>
@@ -41,14 +41,14 @@ export default function Header() {
               href="https://github.com/elifPeriza"
               target="_blank"
               rel="noreferrer"
-              className="w-7 h-7"
+              className="h-7 w-7"
             >
               <Image src={GithubLogo} alt="Go to my LinkedIn" />
             </a>
           </div>
         </div>
-        <div className=" headerTablet:hidden flex flex-row justify-center relative w-full ">
-          <div className=" squircle bg-[url('/elif2.webp')] bg-cover bg-center aspect-square w-[250px] relative "></div>
+        <div className=" relative flex w-full flex-row justify-center headerTablet:hidden ">
+          <div className=" squircle relative h-[200px] w-[200px] bg-[url('/elif2.webp')] bg-cover bg-center sm:h-[220px] sm:w-[220px] md:h-[240px] md:w-[240px] lg:h-[260px] lg:w-[260px] "></div>
         </div>
       </div>
     </>
