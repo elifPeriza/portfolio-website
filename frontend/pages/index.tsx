5f4a65158cb73556a2bf48c32a273861f6499e9c
Elif Gömleksiz <e.goemlek@gmail.com>
2023-03-28 17:08:57 +0200

    merge main into branch fix-types

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,22 +11,22 @@ export default function Web({ articles }: any) {
   return (
     <>
       <div className="bg-violet-bg">
-        <div className=" mx-auto px-[5%] max-w-[1200px]">
+        <div className=" mx-auto max-w-[1200px] px-[5%]">
           <Navbar />
           <Header />
         </div>
-        <div className=" w-full mt-4 md:mt-14">
-          <div className="mx-auto px-[5%] max-w-[1200px]">
-            <h2 className=" underline  decoration-neon-primary decoration-[6px] [text-decoration-skip-ink:none]  font-poppins text-black font-bold text-xl sm:text-2xl sm:pt-10 ">
+        <div className=" mt-4 w-full md:mt-14">
+          <div className="mx-auto max-w-[1200px] px-[5%]">
+            <h2 className=" font-poppins  text-xl font-bold text-black  underline decoration-neon-primary decoration-[6px] [text-decoration-skip-ink:none] sm:pt-10 sm:text-2xl ">
               Work
             </h2>
             <Project />
-            <h2 className="font-poppins font-bold max-w-3xl pt-8 text-black text-2xl mb-3 sm:text-3xl">
+            <h2 className="mb-3 max-w-3xl pt-8 font-poppins text-2xl font-bold text-black sm:text-3xl blogIntroBreakpoint:max-w-[301.5px] ">
               I love to share my learnings through writing, check out my latest
               posts
               <Dot variant="violet" />
             </h2>
-            <h3 className=" underline  decoration-neon-primary decoration-[6px] [text-decoration-skip-ink:none]  font-poppins text-black font-bold text-xl pt-4 sm:text-2xl sm:pt-7 ">
+            <h3 className=" pt-4  font-poppins text-xl font-bold  text-black underline decoration-neon-primary decoration-[6px] [text-decoration-skip-ink:none] sm:pt-7 sm:text-2xl ">
               Writing
             </h3>
 
